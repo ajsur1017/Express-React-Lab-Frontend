fa9a521c30a3816ea08a4e6a75eee7219f9c5794
Anthony <ajsur1017@gmail.com>
2021-10-30 16:36:26 -0400

    cleaning up, pardon the mess

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
-
-// IMPORT COMPONENTS
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Route, Switch } from "react-router-dom";
-// IMPORT PAGES
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 
 function App() {
-  // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
-  const URL = "http://localhost:4000/";
+  const URL = "https://frozen-stream-41139.herokuapp.com/";
 
   return (
     <div className="App">
